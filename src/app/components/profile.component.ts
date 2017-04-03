28db2d58f243df7bad0fe9f0db8f7ef854f89aaf
uzzol101 <davepillowgood@gmail.com>
2017-04-03 19:42:30 +0600

    completed github search app

---
+++ src/app/components/profile.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { GithubService } from '../services/github.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'profile',
+    templateUrl: 'profile.component.html',
+    providers: [GithubService]
+
+})
+export class ProfileComponent {
+    user: Object;
+    repos: any;
+    userName: string;
+    constructor(private githubService: GithubService) {
+
+    }
+
+    searchUser() {
+        this.user = false;
+        this.githubService.updateUser(this.userName);
+
+        this.githubService.getUser().subscribe(user => {
+            this.user = user;
+
+        });
+
+
+        this.githubService.getRepos().subscribe(repo => {
+            this.repos = repo;
+
+        });
+
+
+    }
+}
